fcf71e4020565c06216b695745011f48dead1385
gobbiestyle <gobbie@gstyle.me>
2020-01-27 21:55:38 +0900

    Upload Source Code: pygameMission20-2-1 Upload Source Code: pygameMission20-2-2

---
+++ pygame.Mission20-2-1.py
@@ -0,0 +1,52 @@
+import sys
+import pygame
+import random
+from pygame.locals import QUIT, Rect
+
+pygame.init()
+
+# ------------------------------------ #
+
+class Enemy:
+    def __init__(self):
+        self.image = pygame.image.load("enemy.png")
+        self.width = 30
+        self.height = 30
+        self.x = random.randrange(0, SCREEN_WIDTH - self.width)
+        self.y = 0
+        self.speed = 4
+
+    def draw(self):
+        SCREEN.blit(self.image, Rect(self.x, self.y, self.width, self.height))
+
+# ------------------------------------ #
+
+BLACK = (0, 0, 0)
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 640
+
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("My Game")
+
+CLOCK = pygame.time.Clock()
+FPS = 60
+
+def main():
+    enemy = Enemy()
+
+    while True:
+        SCREEN.fill(BLACK)
+
+        SCREEN.blit(enemy.image, Rect(enemy.x, enemy.y, enemy.width, enemy.height))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        CLOCK.tick(FPS)
+
+if __name__ == '__main__':
+    main()
